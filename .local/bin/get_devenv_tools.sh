f41948e9abbe35e378676f77ff9d93cf255ea86e
Benjamin Kern <261473+BenjaminKern@users.noreply.github.com>
2021-08-15 20:54:34 +0200

    Add bat,vivid,tab,lsd,dust to tool downloader

--- .local/bin/get_devenv_tools.sh
+++ .local/bin/get_devenv_tools.sh
@@ -13,8 +13,13 @@ curl -sL https://github.com/lotabout/skim/releases/download/v0.9.4/skim-v0.9.4-x
 curl -sL https://github.com/sharkdp/hexyl/releases/download/v0.9.0/hexyl-v0.9.0-x86_64-unknown-linux-musl.tar.gz | bsdtar xvfz - --strip-components=1 -C $DESTDIR/bin
 curl -sL https://github.com/sharkdp/hyperfine/releases/download/v1.11.0/hyperfine-v1.11.0-x86_64-unknown-linux-musl.tar.gz | bsdtar xvfz - --strip-components=1 -C $DESTDIR/bin
 curl -sL https://github.com/sharkdp/fd/releases/download/v8.2.1/fd-v8.2.1-x86_64-unknown-linux-musl.tar.gz | bsdtar xvfz - --strip-components=1 -C $DESTDIR/bin
+curl -sL https://github.com/sharkdp/bat/releases/download/v0.18.2/bat-v0.18.2-x86_64-unknown-linux-musl.tar.gz | bsdtar xvfz - --strip-components=1 -C $DESTDIR/bin
+curl -sL https://github.com/sharkdp/vivid/releases/download/v0.7.0/vivid-v0.7.0-x86_64-unknown-linux-musl.tar.gz | bsdtar xvfz - --strip-components=1 -C $DESTDIR/bin
 curl -sL https://github.com/chmln/sd/releases/download/v0.7.6/sd-v0.7.6-x86_64-unknown-linux-musl -o $DESTDIR/bin/sd
 chmod u+x $DESTDIR/bin/sd
 curl -sL https://github.com/starship/starship/releases/download/v0.56.0/starship-x86_64-unknown-linux-musl.tar.gz | bsdtar xvfz - -C $DESTDIR/bin
 curl -sL https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz | bsdtar xvfz - --strip-components=1 -C $DESTDIR
+curl -sL https://github.com/austinjones/tab-rs/releases/download/v0.5.7/tab-x86_64-unknown-linux-musl.tar.gz | bsdtar xvfz - -C $DESTDIR/bin
+curl -sL https://github.com/Peltoche/lsd/releases/download/0.20.1/lsd-0.20.1-x86_64-unknown-linux-musl.tar.gz | bsdtar xvfz - --strip-components=1 -C $DESTDIR/bin
+curl -sL https://github.com/bootandy/dust/releases/download/v0.6.2/dust-v0.6.2-x86_64-unknown-linux-musl.tar.gz | bsdtar xvfz - --strip-components=1 -C $DESTDIR/bin
 # curl -sL https://github.com/Canop/broot/releases/download/v1.6.3/broot_1.6.3.zip| bsdtar xvf - --strip-components=1 -C $DESTDIR
