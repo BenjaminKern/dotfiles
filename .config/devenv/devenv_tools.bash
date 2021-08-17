1995a7ad1cf25a0608b85a817c9607ed6de78c58
Benjamin Kern <261473+BenjaminKern@users.noreply.github.com>
2021-08-17 19:32:55 +0200

    Outline devenv_tools bash config settings

---
+++ .config/devenv/devenv_tools.bash
@@ -0,0 +1,11 @@
+! [[ -v devenv_tools_dir ]] && return
+! [[ -f $devenv_tools_dir/devenv_tools.bash ]] && return
+
+PATH=$devenv_tools_dir/bin:$PATH
+STARSHIP_CONFIG=$devenv_tools_dir/config/starship.toml
+eval "$(starship init bash)"
+LS_COLORS="$(vivid generate one-dark)"
+alias ls='lsd'
+EDITOR=nvim
+
+[[ -v devenv_tools_proxy ]] && HTTP_PROXY=$devenv_tools_proxy && HTTPS_PROXY=$devenv_tools_proxy && http_proxy=$devenv_tools_proxy && https_proxy=$devenv_tools_proxy
