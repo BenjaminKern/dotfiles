b35f253794796bfc1def080020331db6eb3a33fd
Benjamin Kern <261473+BenjaminKern@users.noreply.github.com>
2021-08-23 19:55:27 +0200

    Update devenv source config

--- .config/devenv/devenv_tools.bash
+++ .config/devenv/devenv_tools.bash
@@ -7,5 +7,10 @@ eval "$(starship init bash)"
 LS_COLORS="$(vivid generate one-dark)"
 alias ls='lsd'
 EDITOR=nvim
+alias cat='bat --paging=never'
 
-[[ -v devenv_tools_proxy ]] && HTTP_PROXY=$devenv_tools_proxy && HTTPS_PROXY=$devenv_tools_proxy && http_proxy=$devenv_tools_proxy && https_proxy=$devenv_tools_proxy
+[[ -v devenv_tools_proxy ]] && \
+  HTTP_PROXY=$devenv_tools_proxy && \
+  HTTPS_PROXY=$devenv_tools_proxy && \
+  http_proxy=$devenv_tools_proxy && \
+  https_proxy=$devenv_tools_proxy
