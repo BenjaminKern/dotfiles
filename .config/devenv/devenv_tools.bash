a81e32af12f61f788b6930a57e5352e3ed65cd4c
Benjamin Kern <261473+BenjaminKern@users.noreply.github.com>
2021-09-02 19:47:57 +0200

    Update devenv_tools.bash script

--- .config/devenv/devenv_tools.bash
+++ .config/devenv/devenv_tools.bash
@@ -1,4 +1,4 @@
-! [[ -v devenv_tools_dir ]] && return
+devenv_tools_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ! [[ -f $devenv_tools_dir/devenv_tools.bash ]] && return
 
 PATH=$devenv_tools_dir/bin:$PATH
@@ -8,6 +8,7 @@ LS_COLORS="$(vivid generate one-dark)"
 alias ls='lsd'
 EDITOR=nvim
 alias cat='bat --paging=never'
+eval "$(direnv hook bash)"
 
 [[ -v devenv_tools_proxy ]] && \
   HTTP_PROXY=$devenv_tools_proxy && \
