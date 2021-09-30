a4330d8dc1670f2e0f20858a93f19d6fd23efcbb
Benjamin Kern <261473+BenjaminKern@users.noreply.github.com>
2021-09-30 19:32:49 +0200

    Remove mcfly, add sensible.bash, fzf-keybindings.bash, config fixes

--- .config/devenv/devenv_tools.bash
+++ .config/devenv/devenv_tools.bash
@@ -2,6 +2,10 @@ devenv_tools_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 ! [[ -f $devenv_tools_dir/devenv_tools.bash ]] && return
 
 PATH=$devenv_tools_dir/bin:$PATH
+source $devenv_tools_dir/config/sensible.bash
+source $devenv_tools_dir/config/forgit.plugin.sh
+source $devenv_tools_dir/config/fzf-key-bindings.bash
+export FZF_DEFAULT_COMMAND='fd --type f'
 export STARSHIP_CONFIG=$devenv_tools_dir/config/starship.toml
 eval "$(starship init bash)"
 LS_COLORS="$(vivid generate one-dark)"
@@ -10,13 +14,9 @@ EDITOR=nvim
 alias cat='bat --paging=never'
 eval "$(direnv hook bash)"
 eval "$(zoxide init --cmd j bash)"
-eval "$(mcfly init bash)"
-export MCFLY_KEY_SCHEME=vim
-export MCFLY_FUZZY=true
-source $devenv_tools_dir/forgit.plugin.sh
 
 [[ -v devenv_tools_proxy ]] && \
-  HTTP_PROXY=$devenv_tools_proxy && \
-  HTTPS_PROXY=$devenv_tools_proxy && \
-  http_proxy=$devenv_tools_proxy && \
-  https_proxy=$devenv_tools_proxy
+  export HTTP_PROXY=$devenv_tools_proxy && \
+  export HTTPS_PROXY=$devenv_tools_proxy && \
+  export http_proxy=$devenv_tools_proxy && \
+  export https_proxy=$devenv_tools_proxy
--- .local/bin/get_devenv_tools.sh
+++ .local/bin/get_devenv_tools.sh
@@ -63,12 +63,14 @@ echo "Downloading abduco..."
 curl -sL https://github.com/BenjaminKern/dotfiles/raw/main/.local/pkg/abduco-0.6-musl.tar.gz | bsdtar xfz - -C $DESTDIR/bin
 echo "Downloading zoxide..."
 curl -sL https://github.com/ajeetdsouza/zoxide/releases/download/v0.7.5/zoxide-x86_64-unknown-linux-musl.tar.gz | bsdtar xfz - --strip-components=1 -C $DESTDIR/bin
-echo "Downloading mcfly..."
-curl -sL https://github.com/cantino/mcfly/releases/download/v0.5.9/mcfly-v0.5.9-x86_64-unknown-linux-musl.tar.gz | bsdtar xfz - -C $DESTDIR/bin
 echo "Downloading delta..."
 curl -sL https://github.com/dandavison/delta/releases/download/0.8.3/delta-0.8.3-x86_64-unknown-linux-musl.tar.gz | bsdtar xfz - --strip-components=1 -C $DESTDIR/bin
 echo "Downloading forgit..."
-curl -sL git.io/forgit -o $DESTDIR/forgit.plugin.sh
+curl -sL git.io/forgit -o $DESTDIR/config/forgit.plugin.bash
+echo "Downloading sensible.bash..."
+curl -sL https://raw.githubusercontent.com/mrzool/bash-sensible/master/sensible.bash -o $DESTDIR/config/sensible.bash
+echo "Downloading fzf.keybindings.bash..."
+curl -sL https://raw.githubusercontent.com/junegunn/fzf/master/shell/key-bindings.bash -o $DESTDIR/config/fzf-key-bindings.bash
 echo "Downloading devenv_tools.bash..."
 curl -sL https://raw.githubusercontent.com/BenjaminKern/dotfiles/main/.config/devenv/devenv_tools.bash -o $DESTDIR/devenv_tools.bash
 echo "Downloading starship.toml..."
