a41ad69cf29863c3fa6897bc238b43fd1c6f0b97
Benjamin Kern <261473+BenjaminKern@users.noreply.github.com>
2021-10-08 20:23:41 +0200

    Fix nvim cmp breaking change

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -157,8 +157,8 @@ cmp.setup {
   },
   mapping = {
     ["<Tab>"] = cmp.mapping(function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        feedkey("<C-n>", "n")
+      if cmp.visible() then
+        cmp.select_next_item()
       elseif vim.fn["vsnip#available"]() == 1 then
         feedkey("<Plug>(vsnip-expand-or-jump)", "")
       elseif has_words_before() then
@@ -167,9 +167,10 @@ cmp.setup {
         fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
       end
     end, { "i", "s" }),
+
     ["<S-Tab>"] = cmp.mapping(function()
-      if vim.fn.pumvisible() == 1 then
-        feedkey("<C-p>", "n")
+      if cmp.visible() then
+        cmp.select_prev_item()
       elseif vim.fn["vsnip#jumpable"](-1) == 1 then
         feedkey("<Plug>(vsnip-jump-prev)", "")
       end
