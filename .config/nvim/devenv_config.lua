17156a5460e74f6de952a424a2058c325bf3fcd7
Benjamin Kern <261473+BenjaminKern@users.noreply.github.com>
2022-06-02 10:19:38 +0200

    Fix regressions

--- .config/nvim/devenv_config.lua
+++ .config/nvim/devenv_config.lua
@@ -178,10 +178,10 @@ require('hop').setup({
 vim.keymap.set('n', 'ww', function()
   require('hop').hint_words()
 end, { desc = 'Use hop' })
-vim.g.nvim_tree_respect_buf_cwd = 1
 
 require('nvim-tree').setup({
   update_cwd = true,
+  respect_buf_cwd = true,
   update_focused_file = {
     enable = true,
     update_cwd = true,
