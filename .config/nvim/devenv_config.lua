d8b1f0ab1baa907956eb2ac263cc362d6dc5e902
Benjamin Kern <261473+BenjaminKern@users.noreply.github.com>
2022-10-02 22:09:26 +0200

    Add mini.surround to config

--- .config/nvim/devenv_config.lua
+++ .config/nvim/devenv_config.lua
@@ -258,6 +258,7 @@ require('mini.sessions').setup({
   verbose = { write = true, delete = true },
 })
 require('mini.align').setup()
+require('mini.surround').setup()
 require('mini.pairs').setup()
 require('mini.fuzzy').setup()
 require('mini.pairs').unmap('i', '"', '""')
